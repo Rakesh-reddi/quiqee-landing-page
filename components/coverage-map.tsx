2b4e977d4a1e85c6fb9b903b40327cdad519b044
tsumith <tsumith987@gmail.com>
2026-04-05 16:58:45 +0530

    feat(map): replace static coverage image with interactive Leaflet map

---
+++ components/coverage-map.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+
+//  fixes Next.js not finding the default map pin icons
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
+
+export default function CoverageMap() {
+    const position: [number, number] = [17.4764, 78.2139]
+    return (
+    <MapContainer 
+      center={position} 
+      zoom={14} 
+      scrollWheelZoom={true}
+      className="w-full h-full z-0"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          <strong>Quiqee Coverage Center</strong><br />
+          Serving Mokila & 5km radius.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  )
+}
